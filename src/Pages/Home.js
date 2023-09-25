14d9e0d337f1620bb8241c7a6e09cc4dacecb9f5
Aditya Agrawal <43040075+aditya7496@users.noreply.github.com>
2023-09-25 17:08:09 -0300

    Home.js changes made.

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -8,13 +8,14 @@ const Home = () => {
         largeHeader="Desi Food Stuff, Spice Store Is Bringing Home Closer To You."
         mediumHeader="Looking for the right brands and the right flavours, visit The Spice Store and find all your favorite foods at competitive prices."
       />
+      <button>Find A Location Near you</button>
       <HeroComponent
         largeHeader="What Makes Us Different"
-        mediumHeader="Brands both loved and found at home from trusted suppliers, consistency, competitive prices, quality service, and cleanliness."
+        mediumHeader="Brands both loved and found at home from trusted suppliers, we believe in providing our customers with SPICE experience which stands for great Service at the best Prices in an Inviting and Clean Environment.
+        Find us in your neighborhood soon with out fast growing number of store we hope to bring you food and comfort in as many neighborhoods and provinces in Canada."
       />
       <div className="large-header"></div>
       <div className="medium-header"></div>
-      <button>Find A Location Near you</button>
     </div>
   );
 };
